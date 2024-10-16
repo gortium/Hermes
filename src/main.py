c0549fb703ae09d59e9d07f3aca4f16a3c214fe0
Thierry Pouplier <thierrypouplier@gmail.com>
2024-10-15 22:54:21 -0400

    Simple langgraph graph

--- src/main.py
+++ src/main.py
@@ -1,6 +1,10 @@
 import os
 from dotenv import load_dotenv
 from langchain_ollama import ChatOllama
+from typing import Annotated
+from typing_extensions import TypedDict
+from langgraph.graph import StateGraph, START, END
+from langgraph.graph.message import add_messages
 
 # Load environment variables from .env file
 load_dotenv()
@@ -9,10 +13,17 @@ llm_model = os.getenv('MODEL')
 
 llm = ChatOllama(model=llm_model, base_url=ollama_url, temperature=0)
 
+class State(TypedDict):
+    messages: Annotated[list, add_messages]
+
+def chatbot(state: State):
+    return {"messages": [llm.invoke(state["messages"])]}
+
+graph_builder = StateGraph(State)
+graph_builder.add_node("chatbot", chatbot)
+graph_builder.add_edge(START, "chatbot")
+graph_builder.add_edge("chatbot", END)
+
 if __name__ == "__main__":
-    response = llm.invoke([
-        ("system", "You are a helpful translator. Translate the user sentence to French."),
-        ("human", "I love programming."),
-        ])
-    
-    print(response.content)
+    graph = graph_builder.compile()
+    print(graph.get_graph().draw_ascii())
