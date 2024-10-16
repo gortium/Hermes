3974757e7df4296e9f9125356956c75318cf8587
Thierry Pouplier <thierrypouplier@gmail.com>
2024-10-15 23:29:04 -0400

    simple langgraph chatbot

--- src/main.py
+++ src/main.py
@@ -11,7 +11,7 @@ load_dotenv()
 ollama_url = os.getenv('OLLAMA_URL')
 llm_model = os.getenv('MODEL')
 
-llm = ChatOllama(model=llm_model, base_url=ollama_url, temperature=0)
+llm = ChatOllama(model=llm_model, base_url=ollama_url, temperature=0, stream=True)
 
 class State(TypedDict):
     messages: Annotated[list, add_messages]
@@ -24,6 +24,26 @@ graph_builder.add_node("chatbot", chatbot)
 graph_builder.add_edge(START, "chatbot")
 graph_builder.add_edge("chatbot", END)
 
+def stream_graph_updates(user_input: str):
+    for event in graph.stream({"messages": [("user", user_input)]}):
+        for value in event.values():
+            print("Assistant:", value["messages"][-1].content)
+
 if __name__ == "__main__":
     graph = graph_builder.compile()
     print(graph.get_graph().draw_ascii())
+    
+    while True:
+        try:
+            user_input = input("User: ")
+            if user_input.lower() in ["quit", "exit", "q"]:
+                print("Goodbye!")
+                break
+
+            stream_graph_updates(user_input)
+        except:
+            # fallback if input() is not available
+            user_input = "What do you know about LangGraph?"
+            print("User: " + user_input)
+            stream_graph_updates(user_input)
+            break
